7a896bf4093549088beb5ca5ccf9952e2b037fdb
josephwele <josiwele@gmail.com>
2019-04-06 13:22:12 -0700

    js modified

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,10 +14,24 @@ firebase.initializeApp(config);
 
 var dataRef = firebase.database();
 // Initial Values
-var emploName= "";
-var role = "";
-var date = "" ;
-var rate = 0;
-document.querySelector("#btnname").addEventListener("click", function(event) {
-  event.preventDefault();
-}
+var emploName= "", 
+role = "",
+date = "" ,
+monthRate= 0;
+document.querySelector("#teamWork").addEventListener("click", function(event) {
+  event.preventDefault(); 
+  // get value from input
+  name = document.querySelector("#name-input").value.trim();
+  role = document.querySelector("#tole-input").value.trim();
+  monthRate = document.querySelector("#month-input").value.trim();
+  date = document.querySelector("#date-input").value.trim();
+  // push the iput
+  dataRef.ref().push({
+    name: name,
+    email: role,
+    monthRate: monthRate,
+    comment: comment,
+    date:date;
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+});
